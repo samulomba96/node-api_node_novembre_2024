aadd97aa9d292b9816b7eb7186993aa2c69d36b7
Matteo Valenzi <dilloawallace@hotmail.com>
2024-11-21 21:14:18 +0100

    express

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,7 @@
-console.log("App Works!");
+import express, { Request, Response, NextFunction } from "express";
+import { config } from "dotenv";
+config();
+
+const app = express();
+
+app.listen(process.env.PORT ?? 3000, () => console.log("FUNZIONA!"));
